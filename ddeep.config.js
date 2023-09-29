5fa70819c523f4a3ec821d049836e7f840091678
kais-radwan <kais.radwan.personal@gmail.com>
2023-09-29 18:33:07 +0300

    adding config files

---
+++ ddeep.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    // Set storage to true to have persistent storage
+    "storage": true,
+
+    // Set the port you want to run the peer on
+    "port": 9999,
+
+    // set logs to false if you don't want to see real-tiem logs in your peer
+    "logs": true
+
+}
---
+++ policies.config.js
@@ -0,0 +1,20 @@
+
+module.exports = [
+
+    {
+        name: "auth_policy",
+        operations: ["read", "write"],
+        type: "check",
+        graph: "people/kais",
+
+        check(instance, data) {
+
+            if (instance.auth() === data.auth()) return true;
+
+            if (instance.auth() !== data.auth()) return false;
+
+        }
+
+    }
+
+]
